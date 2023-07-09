2e915f459e314f04523a3ef250df120582b84592
OMmienu <nkedzi@gmail.com>
2023-07-09 16:49:55 -0700

    0-lockboxes.py

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,17 +1,35 @@
 #!/usr/bin/python3
-"""lockboxes challenge"""
+"""lockboxes
+"""
 
 
-def canUnlockAll(newList):
-    """lock box function"""
-    pack = [0]
-    for x in range(len(newList)):
-        for y in newList[x]:
-            if not (y == x):
-                pack.append(y)
-        if x not in pack:
-            for z in range(x+1, len(newList)):
-                [pack.append(k) for k in newList[z]]
-            if x not in pack:
-                return False
-    return True
+def canUnlockAll(boxes):
+    """determines if all the boxes can be opened
+    """
+
+    available_boxes = list(range(len(boxes)))
+    visited_boxes = []
+    queue = [0]
+
+    while len(queue) != 0:
+        key = queue.pop(0)
+
+        # Check if key has a box
+        if key in available_boxes:
+            if key in visited_boxes:
+                # skip key
+                continue
+            else:
+                # Add keys in the box to queue
+                queue.extend(boxes[key])
+                visited_boxes.append(key)
+        else:
+            # skip key
+            continue
+
+        # optimise by checking if all boxes have been unlocked
+        # but queue is not empty
+        if (set(visited_boxes) == set(available_boxes)):
+            return True
+
+    return set(visited_boxes) == set(available_boxes)
